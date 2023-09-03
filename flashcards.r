e89e3251c76c63477ee054b906d8f63a5550d0b4
Sabina Kaczor <sabinakaczor.dev@gmail.com>
2023-09-03 19:55:02 +0200

    Add different modes

--- flashcards.r
+++ flashcards.r
@@ -1,12 +1,19 @@
-play <- function() {
+play <- function(from = "") {
   df <- read.table("flashcards.txt", sep = "\t", header = TRUE)
   while (TRUE) {
     indices <- sample(seq_len(nrow(df)))
     for (index in indices) {
+      if (from != "") {
+        to <- if (from == "PL") "EN" else "PL"
+      } else {
+        dir <- sample(c("PL", "EN"))
+        from <- dir[1]
+        to <- dir[2]
+      }
       row <- df[index, ]
-      message(row$PL, appendLF = FALSE)
+      message(row[[from]], appendLF = FALSE)
       readline()
-      message(" -> ", row$EN, "\n")
+      message(" -> ", row[[to]], "\n")
     }
   }
 }
